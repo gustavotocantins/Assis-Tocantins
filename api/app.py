a944cdfbfa6df8003cacfd61f963c524c2636c7d
Gustavo Tocantins <gustahofc@gmail.com>
2024-01-19 23:30:52 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from PIL import Image
 import numpy as np
 from pickle import load
@@ -15,6 +15,10 @@ def index():
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
+@app.route('/whatsapp')
+def whatsapp():
+    return redirect('https://chat.whatsapp.com/Ghx4tk2ApIw9A9WNTklIYF')
 
 if __name__ == '__main__':
     app.run()
